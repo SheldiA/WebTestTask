8bb7f1b404fe2a2e4655ed156bfc1426b6488496
Ann <banadena@gmail.com>
2014-03-16 21:34:29 +0300

    add regex for checking datetime

--- WebsiteTestTask/Scripts/jquery_form_validation.js
+++ WebsiteTestTask/Scripts/jquery_form_validation.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
+    $.validator.addMethod("datetime", function (value, element) {
+        return /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(19|20)\d\d\s([01]\d|2[0-3])(:[0-5]\d){2}$/.test(value);
+    }, "Enter date-time in format DD.MM.YYYY HH:MM:SS");
     $("#fm_usersEditor").validate({
         rules: {
             dv_userssList$tb_name: {
@@ -14,9 +17,12 @@
                 required:true,
                 email:true,
             },
-            dv_userssList$tb_start_date: {
-                date:true
-            },
+            dv_userssList$tb_start_date: "datetime",
+        },
+        highlight: function (element, errorClass) {
+            $(element).fadeOut(function () {
+                $(element).fadeIn();
+            });
         },
         success: function (label) {
             label.text('OK!').addClass('valid');
